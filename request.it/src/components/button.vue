4375669642b8e44b2c339fec1b484bf5339bbfbb
Salomao Neto <salomaosnff@yahoo.com>
2021-04-29 21:02:03 -0300

    Botões

--- request.it/src/components/button.vue
+++ request.it/src/components/button.vue
@@ -1,5 +1,10 @@
 <template>
-  <button class="r-button"><slot></slot></button>
+  <button :class="['r-button', {
+    'r-button--block': block,
+    'r-button--secondary': secondary,
+  }]">
+    <slot></slot>
+  </button>
 </template>
 
 <style>
@@ -8,16 +13,39 @@
   color: var(--vscode-button-foreground);
   border: none;
   outline: none;
+  padding: 0.4em 1em;
+  margin: 0.25em;
+  cursor: pointer;
+}
+
+.r-button--block {
+  display: block;
+  width: 100%;
 }
 
 .r-button:hover {
   background: var(--vscode-button-hoverBackground);
 }
+
+.r-button--secondary {
+  background: var(--vscode-button-secondaryBackground);
+  color: var(--vscode-button-secondaryForeground);
+}
+
+.r-button--secondary:hover {
+  background: var(--vscode-button-secondaryHoverBackground);
+}
 </style>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
 
 @Component
-export default class extends Vue {}
+export default class extends Vue {
+  @Prop(Boolean)
+  secondary !: boolean
+
+  @Prop(Boolean)
+  block !: boolean
+}
 </script>
---
+++ request.it/src/views/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <r-button>Nova requisição</r-button>
+    <r-button secondary>Nova requisição</r-button>
+    <r-button block>Nova requisição</r-button>
+    <r-button secondary block>Nova requisição</r-button>
+    <!-- <div class="r-tabs">
+      <r-tab-link>Histórico</r-tab-link>
+      <r-tab-link>Coleções</r-tab-link>
+      <r-tab-link>Variáveis</r-tab-link>
+    </div> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import RButton from "@/components/button.vue";
+import RTabLink from "@/components/TabLink.vue";
+
+@Component({
+  components: { RButton, RTabLink },
+})
+export default class Home extends Vue {}
+</script>
+<style>
+.r-tabs {
+  border-bottom: 2px solid red;
+}
+</style>
